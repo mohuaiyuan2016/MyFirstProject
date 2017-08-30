f2602868a764a9a5385e48cdde50b274e7b4aecb
mohuaiyuan <1070605879@qq.com>
2017-08-30 14:59:36 +0800

    Second commit --20170830 15:00 code backup

--- app/src/main/java/com/example/myfirstproject/MainActivity.java
+++ app/src/main/java/com/example/myfirstproject/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.myfirstproject;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    private Context context;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context=this;
+
     }
 }
